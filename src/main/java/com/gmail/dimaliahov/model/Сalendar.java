50c7d435c3f326f0d66f3608c59dd9d59f9f9084
LiakhovDmitriy <dimaliahov@gmail.com>
2020-07-01 11:19:37 +0300

    1

---
+++ src/main/java/com/gmail/dimaliahov/model/Сalendar.java
@@ -0,0 +1,19 @@
+package com.gmail.dimaliahov.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Сalendar {
+    private int id;
+    private Date date;
+//    Создать ліст в якому будуть лежить зайняті дати для одного викладача
+//    OneToOne
+//
+
+}
