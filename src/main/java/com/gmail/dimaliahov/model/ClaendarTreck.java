eb45eceff176204f14ab7b397d3abbb0a0da383d
LiakhovDmitriy <dimaliahov@gmail.com>
2020-07-01 11:24:08 +0300

    1

--- src/main/java/com/gmail/dimaliahov/model/ClaendarTreck.java
+++ src/main/java/com/gmail/dimaliahov/model/ClaendarTreck.java
@@ -9,11 +9,11 @@ import java.util.Date;
 @Getter
 @Setter
 @AllArgsConstructor
-public class Сalendar {
-    private int id;
-    private Date date;
+public class ClaendarTreck {
+    
+        private int id;
+        private Date date;
 //    Создать ліст в якому будуть лежить зайняті дати для одного викладача
 //    OneToOne
 //
-
 }
